73b0919254b9e6348bfdfad0c106323095fc63d5
sameermd11 <89958787+sameermd11@users.noreply.github.com>
2021-09-27 01:15:39 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Subex</title>
+    <title>Quient Training</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
